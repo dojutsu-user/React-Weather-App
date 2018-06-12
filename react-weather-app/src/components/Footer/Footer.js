043774260eef2f750b36ac7b9bcf6d49ef2082a6
dojutsu-user <vaibhgupt199@gmail.com>
2018-06-12 20:38:19 +0530

    Add Github Logo

--- react-weather-app/src/components/Footer/Footer.js
+++ react-weather-app/src/components/Footer/Footer.js
@@ -5,9 +5,13 @@ import cssClass from "./Footer.css";
 const footer = () => {
     return (
         <div className={cssClass.Color}>
-            Made With ❤️ By{" "} 
+            Made With ❤️ By{" "}
             <a className={cssClass.Name} href="https://github.com/dojutsu-user">
                 Vaibhav Gupta
+            </a>{" "}
+            |{" "}
+            <a className={cssClass.Github} href="https://github.com/dojutsu-user/React-Weather-App">
+                <i class="fab fa-github fa-1x" />
             </a>
         </div>
     );
