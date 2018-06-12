e72fefe9082c0d3031e3071cc02aa9894b7e0b46
dojutsu-user <vaibhgupt199@gmail.com>
2018-06-12 12:23:37 +0530

    Make SearchForm Component

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 
 import "./App.css";
 import Title from "./components/Title/Title";
+import SearchForm from "./containers/SearchForm/SearchForm";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Title />
+                <SearchForm />
             </div>
         );
     }
---
+++ react-weather-app/src/containers/SearchForm/SearchForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SearchForm extends Component {
+    render() {
+        return (
+            <div>
+                <p>Search Here</p>
+            </div>
+        );
+    }
+}
+
+export default SearchForm;
